7e28a3bfede48a8921abd53b7a85e2d976f73a8f
djanloo <zzibecu@gmail.com>
2022-11-09 00:40:09 +0100

    minor

--- profile.py
+++ profile.py
@@ -13,15 +13,15 @@ chdir(join(dirname(__file__), "cryd"))
 
 profile = LineProfiler()
 
-profile.add_function(ercolation.run_by_cells)
+profile.add_function(ercolation.shells_by_cells)
 
 S = np.random.uniform(0,1 , size=(1000, 3)).astype(np.float32)
 
 np.random.seed(42)
 
-wrap = profile(ercolation.run_by_cells)
+wrap = profile(ercolation.shells_by_cells)
 for j in range(10):
     ercolation.set_seed(42)
-    print(wrap(S, 0.1))
+    print(wrap(S, 0.1, 0.1, 0.8, 0.8))
 
 profile.print_stats()
